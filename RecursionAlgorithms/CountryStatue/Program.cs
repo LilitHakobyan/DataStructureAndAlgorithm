47d554de5dbb9db89ffae1c3f509e90f3bd3db84
lilithakobyan <lilithakobyan1994@mail.ru>
2018-02-28 14:46:01 +0400

    CountryStatue

---
+++ RecursionAlgorithms/CountryStatue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountryStatue
+{
+    class Program
+    {
+        static int CountryArdz(int n, int k)
+        {
+            if (k > n) return 0;
+            if (k == n || k == 0) return 1;
+            else
+                return CountryArdz(n - 1, k - 1) +
+                       CountryArdz(n - 1, k);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
